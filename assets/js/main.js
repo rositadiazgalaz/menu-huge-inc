d4aa4bd1f472f2ba670111fccec86bc101f6c9d6
rositadiazgalaz <rositadiazgalaz@gmail.com>
2017-05-11 22:48:51 -0300

    avance

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+document.getElementById("toggle").addEventListener("click", function(event){
+	event.preventDefault();
+	document.getElementById("header").classList.toggle("open");
+	document.getElementById("body").classList.toggle("overflow-hidden");
+});
+
+
+var lastScrollTop = 0;
+window.addEventListener("scroll", function(){
+	var currentScroll = window.pageYOffset || document.body.scrollTop;
+	if(currentScroll > lastScrollTop){
+		document.getElementById("header").classList.remove("header-in");
+		document.getElementById("header").style.opacity=0;	
+	}else{
+		document.getElementById("header").classList.add("header-in");
+		document.getElementById("header").classList.add("solid");
+		document.getElementById("header").style.opacity = 1;
+	}
+	if (currentScroll<=3) {
+		document.getElementById("header").classList.remove("solid");
+		document.getElementById("header").classList.remove("header-in");
+	}
+	lastScrollTop = currentScroll;
+}, false);
